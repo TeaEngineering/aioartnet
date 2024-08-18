8a9cf74d233526170826551ce03ae52d036b901e
Chris Shucksmith <chris@shucksmith.co.uk>
2024-08-18 10:32:17 +0100

    fixup: network formatting

--- aioartnet/network.py
+++ aioartnet/network.py
@@ -1,46 +1,49 @@
 import ctypes.util
+import logging
 import os
 import socket
-import logging
-from sys import platform
-from typing import Optional
-
 from ctypes import (
     CDLL,
     POINTER,
     Structure,
-    c_char,
     c_char_p,
     c_int,
     c_uint,
     c_uint8,
-    c_ushort,
     c_void_p,
     pointer,
 )
+from sys import platform
+from typing import Optional
 
 if platform == "linux" or platform == "linux2":
+
     class Sockaddr(ctypes.Structure):
         _fields_ = [
-            ('sa_family', ctypes.c_uint16),
-            ('sa_data', ctypes.c_uint8 * (2+4+16))
+            ("sa_family", ctypes.c_uint16),
+            ("sa_data", ctypes.c_uint8 * (2 + 4 + 16)),
         ]
+
     ipv4_addr_data_offset = 2
     AF_PACKET = socket.AF_PACKET
 elif platform == "darwin":
+
     class Sockaddr(Structure):
         _fields_ = [
             ("sa_len", c_uint8),
             ("sa_family", c_uint8),
-            ("sa_data", c_uint8*24),
+            ("sa_data", c_uint8 * 24),
         ]
+
     ipv4_addr_data_offset = 2
     AF_PACKET = 17
 else:
     raise ValueError("Unsupported platform")
 
+
 class Ifaddrs(Structure):
     pass
+
 
 Ifaddrs._fields_ = [
     ("ifa_next", POINTER(Ifaddrs)),
@@ -53,7 +56,7 @@ Ifaddrs._fields_ = [
 ]
 
 
-def getifaddrs(ifname: Optional[str]=None, family: Optional[int]=None):
+def getifaddrs(ifname: Optional[str] = None, family: Optional[int] = None):
     libc = CDLL(
         ctypes.util.find_library("socket" if os.uname()[0] == "SunOS" else "c"),
         use_errno=True,
@@ -71,7 +74,7 @@ def getifaddrs(ifname: Optional[str]=None, family: Optional[int]=None):
         broadaddr = None
         addr = None
         fam = ifaddr_p.contents.ifa_addr.contents.sa_family
-        d = {'name': name, 'family': fam}
+        d = {"name": name, "family": fam}
         if ifaddr_p.contents.ifa_broadaddr:
             broadaddr = bytes(ifaddr_p.contents.ifa_broadaddr.contents.sa_data)
         if ifaddr_p.contents.ifa_netmask:
@@ -80,15 +83,15 @@ def getifaddrs(ifname: Optional[str]=None, family: Optional[int]=None):
             addr = bytes(ifaddr_p.contents.ifa_addr.contents.sa_data)
 
         if fam == socket.AF_INET:
-            d['addr'] = socket.inet_ntoa(addr[2:6])
-            d['netmask'] = socket.inet_ntoa(netmask[2:6])
-            d['broadaddr'] = socket.inet_ntoa(broadaddr[2:6])
+            d["addr"] = socket.inet_ntoa(addr[2:6])
+            d["netmask"] = socket.inet_ntoa(netmask[2:6])
+            d["broadaddr"] = socket.inet_ntoa(broadaddr[2:6])
         elif fam == socket.AF_INET6:
-            d['addr'] = socket.inet_ntop(fam, addr[6:22])
-            d['netmask'] = socket.inet_ntop(fam, netmask[6:22])
+            d["addr"] = socket.inet_ntop(fam, addr[6:22])
+            d["netmask"] = socket.inet_ntop(fam, netmask[6:22])
         elif fam == AF_PACKET:
-            d['addr'] = addr[10:16].hex()
-            d['broadaddr'] = broadaddr[10:16].hex()
+            d["addr"] = addr[10:16].hex()
+            d["broadaddr"] = broadaddr[10:16].hex()
         logging.debug(f"getifaddrs {d}")
 
         ifaddr_p = ifaddr_p.contents.ifa_next
